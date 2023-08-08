c343fad72d1f331143d31d06eeb0bffefa6847ad
ThaUnknown <6506529+ThaUnknown@users.noreply.github.com>
2023-08-08 13:04:06 +0200

    feat: types

--- index.js
+++ index.js
@@ -1,11 +1,20 @@
+// @ts-check
 import mime from 'mime/lite.js'
 
+/**
+ * @class
+ * @implements {File}
+*/
 export default class URLFile {
-  constructor (url, size, { type, lastModifiedDate, lastModified, name, fetch = globalThis.fetch, _size } = {}, { start = 0, end = size } = {}) {
+  /**
+   * @param {string | URL} url
+   * @param {number} size
+   */
+  constructor (url, size, { type = '', lastModifiedDate = new Date(), lastModified = 0, name = null, fetch = globalThis.fetch, _size = size } = {}, { start = 0, end = size } = {}) {
     if (size == null || isNaN(size)) throw new Error('Size is required!')
     this.url = new URL(url)
     this.size = size // instance size - sliced, fake
-    this._size = _size ?? size // true remote file size
+    this._size = _size // true remote file size
     const hasExtension = this.url.pathname.lastIndexOf('.') !== -1
     this.name = name ?? hasExtension ? this.url.pathname.slice(this.url.pathname.lastIndexOf('/') + 1, this.url.pathname.lastIndexOf('.')) : this.url.pathname.slice(this.url.pathname.lastIndexOf('/') + 1)
     this.type = type ?? hasExtension ? mime.getType(this.name) : ''
@@ -27,10 +36,10 @@ export default class URLFile {
   }
 
   /**
-   * @param  {Number} start slice start position, inclusive
-   * @param  {Number} end=this.size slice end position, non-inclusive, W3C slice's end is non-inclusive, but HTTP's and Node's ends are inclusive, be careful!!!
+   * @param  {Number=} start slice start position, inclusive
+   * @param  {Number=} end=this.size slice end position, non-inclusive, W3C slice's end is non-inclusive, but HTTP's and Node's ends are inclusive, be careful!!!
    */
-  slice (start, end = this.size) {
+  slice (start = 0, end = this.size) {
     if (start == null || this.size === 0) return this
     if (end < 0) end = Math.max(this.size + end, 0)
     if (start < 0) start = Math.max(this.size + start, 0)
@@ -52,26 +61,24 @@ export default class URLFile {
   stream () {
     if (this.size === 0) return new ReadableStream()
     const ts = new TransformStream()
-    this._get().then(res => res.body.pipeTo(ts.writable))
+    this._get().then(res => res.body?.pipeTo(ts.writable))
     return ts.readable
   }
 
   async text () {
     if (this.size === 0) return ''
     const res = await this._get()
-    const text = await res.text()
-    return text
+    return await res.text()
   }
 
   async arrayBuffer () {
-    if (this.size === 0) return new ArrayBuffer()
+    if (this.size === 0) return new ArrayBuffer(0)
     const res = await this._get()
-    const ab = await res.arrayBuffer()
-    return ab
+    return await res.arrayBuffer()
   }
 
   async blob () {
-    if (this.size === 0) return new Blob([], { type: this.type })
+    if (this.size === 0) return new Blob([], { type: this.type ?? '' })
     const res = await this._get()
     return await res.blob()
   }
@@ -83,5 +90,5 @@ export async function fromURL (url) {
   const { headers } = await fetch(url, { method: 'HEAD' })
   if (headers.get('accept-ranges') === 'none') throw new Error('Range requests not supported by remote')
   const lastModifiedDate = new Date(headers.get('last-modified') ?? Date.now())
-  return new URLFile(url, Number(headers.get('content-length')), { type: headers.get('content-type'), lastModifiedDate, lastModified: +lastModifiedDate })
+  return new URLFile(url, Number(headers.get('content-length')), { type: headers.get('content-type') ?? '', lastModifiedDate, lastModified: +lastModifiedDate })
 }
